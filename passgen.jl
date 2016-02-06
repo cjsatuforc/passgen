fc6f3b56cac11ca9f2853b90fbf79f5d30e2b86a
Jacob Doyelle Yates <blmvxer@gmail.com>
2016-02-06 16:31:17 -0500

    Update passgen.jl

--- passgen.jl
+++ passgen.jl
@@ -1,10 +1,8 @@
-#Work In Progress#
-#Passgen port to julia#
-function gen_lower()
+function gen_pass()
         v = ARGS[2]
         f_set = Any[]
         while length(f_set) != int(v)
-                for i in rand(1:26)
+                for i in rand(1:z)
                         for x in c_set[i]
                                 push!(f_set, x)
                         end
@@ -17,14 +15,14 @@ end
 while true
         if ARGS[1] == "-l"
                 global c_set = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-                gen_lower()
+                global z = length(c_set)
+                gen_pass()
         elseif ARGS[1] == "-u"
                 global c_set = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-                gen_lower()
+                global z = length(c_set)
+                gen_pass()
         else
-                println("Arguments: -l [num]")
+                println("Arguments: -l [num], -u [num]")
                 break
         end
 end
-
-
