d71deb1f7c6eb92652d3d702080f37a4db064877
Jacob Doyelle Yates <blmvxer@gmail.com>
2016-02-06 16:25:28 -0500

    Update passgen.jl

--- passgen.jl
+++ passgen.jl
@@ -1,8 +1,7 @@
 #Work In Progress#
 #Passgen port to julia#
 function gen_lower()
-        v = ARGS[1]
-        c_set = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+        v = ARGS[2]
         f_set = Any[]
         while length(f_set) != int(v)
                 for i in rand(1:26)
@@ -16,6 +15,16 @@ println(z_set)
 end
 
 while true
-        gen_lower()
+        if ARGS[1] == "-l"
+                global c_set = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+                gen_lower()
+        elseif ARGS[1] == "-u"
+                global c_set = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+                gen_lower()
+        else
+                println("Arguments: -l [num]")
+                break
+        end
 end
+
 
