1d379a05230fb8b85f2f5a9a9df2d700a774d5e8
Jacob Doyelle Yates <blmvxer@gmail.com>
2016-02-06 17:05:03 -0500

    Update passgen.jl

--- passgen.jl
+++ passgen.jl
@@ -12,33 +12,36 @@ z_set = join(f_set)
 println(z_set)
 end
 
+lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 while true
 	if ARGS[1] == "-l"
-		global c_set = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+		global c_set = lower
 		global z = length(c_set)
 		gen_pass()
 	elseif ARGS[1] == "-u"
-		global c_set = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+		global c_set = upper
 		global z = length(c_set)
 		gen_pass()
 	elseif ARGS[1] == "-lu"
-		global c_set = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+		global c_set = vcat(lower, upper)
 		global z = length(c_set)
 		gen_pass()
 	elseif ARGS[1] == "-n"
-		global c_set = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+		global c_set = num
 		global z = length(c_set)
 		gen_pass()
 	elseif ARGS[1] == "-ln"
-                global c_set = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+                global c_set = vcat(lower, num)
                 global z = length(c_set)
                 gen_pass()
 	elseif ARGS[1] == "-un"
-                global c_set = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+                global c_set = vcat(upper, num)
                 global z = length(c_set)
                 gen_pass()
     elseif ARGS[1] == "-lun"
-		global c_set = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+		global c_set = vcat(lower, upper, num)
 		global z = length(c_set)
 		gen_pass()
 	else
@@ -46,4 +49,3 @@ while true
 		break
 	end
 end
-
