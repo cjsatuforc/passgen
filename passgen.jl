12f6a42f7ded421a3ade91e43d8cbbce653c81df
Jacob Doyelle Yates <blmvxer@gmail.com>
2016-03-16 21:43:49 -0400

    Removing bloat

--- passgen.jl
+++ passgen.jl
@@ -7,6 +7,7 @@ end
 
 function gen_pass()
 	v = ARGS[2]
+	global z = length(c_set)
 	f_set = Any[]
 	while length(f_set) != parse(Int, v)
 		for i in rand(1:z)
@@ -48,35 +49,27 @@ b32 = ['2', '3', '4', '5', '6', '7']
 while true
 	if ARGS[1] == "-l"
 		global c_set = lower
-		global z = length(c_set)
 		gen_pass()
 	elseif ARGS[1] == "-u"
 		global c_set = upper
-		global z = length(c_set)
 		gen_pass()
 	elseif ARGS[1] == "-lu"
 		global c_set = vcat(lower, upper)
-		global z = length(c_set)
 		gen_pass()
 	elseif ARGS[1] == "-n"
 		global c_set = num
-		global z = length(c_set)
 		gen_pass()
 	elseif ARGS[1] == "-ln"
 		global c_set = vcat(lower, num)
-		global z = length(c_set)
 		gen_pass()
 	elseif ARGS[1] == "-un"
 		global c_set = vcat(upper, num)
-		global z = length(c_set)
 		gen_pass()
 	elseif ARGS[1] == "-lun"
 		global c_set = vcat(lower, upper, num)
-		global z = length(c_set)
 		gen_pass()
 	elseif ARGS[1] == "-b32"
 		global c_set = vcat(lower, b32)
-		global z = length(c_set)
 		gen_pass()
 	else
 		println("Arguments: -l [num], -u [num]")
