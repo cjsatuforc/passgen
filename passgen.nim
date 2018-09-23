4255f2954fd5ea10330a594065a5b0ff64032618
Jacob Doyelle Yates <blmvxer@gmail.com>
2018-09-23 18:54:34 -0400

    Look elsewhere...
    Not ready for prime time.

--- passgen.nim
+++ passgen.nim
@@ -2,16 +2,24 @@
 import os, strutils, hashes, random, parseopt
 
 #Compiler options
-{.deadCodeElim:on, checks:off, hints:off, warnings:off, optimization:speed.}
+{.deadCodeElim:on, checks:off, hints:off, warnings:off.}
+
 var
   asciiGen = @["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
   digitGen = @["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-proc main =
-  proc genHash(asciiPick: string lengthPick: int): string {.discardable.} =
-    randomize()
-    of cmdLongOption:
-      case key
-      of "u", "l", "d", "ul", "ud", "ld", "uld":
+  hashGen = newSeq[string](0)
 
-      case value
-      of lengthPick
+
+echo "Hash Length: "
+let hashLen = readLine(stdin)
+
+proc main =
+  randomize()
+  var v = 0
+  for v in countup(0, parseInt(hashLen) - 1):
+    shuffle(asciiGen)
+    hashGen.add(asciiGen[v])
+  let hashString = join(hashGen)
+  echo hashString
+main()
+
